0cef2c6b202d5eaf1eeb1dd6583ad26c1d706fed
shekanson <shekanson88@gmail.com>
2021-06-18 15:58:18 +0800

    update css

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var blueScore = 0, redScore = 0;
+var numOfArrow = new Array(2);
+ 
+// Loop to create 2D array using 1D array
+for (var i = 0; i < numOfArrow.length; i++) {
+    numOfArrow[i] = new Array(5);
+}
+
 function add1(pot){
     var count = document.getElementById(pot);
     var number = parseInt(count.textContent);
@@ -28,8 +36,8 @@ function updateBlue(){
     number[3] = parseInt(document.getElementById("b4").textContent);
     number[4] = parseInt(document.getElementById("b5").textContent);
 
-    var score = 0, i = 0;
-    for(i = 0; i < 5; i++){
+    var score = 0;
+    for(var i = 0; i < 5; i++){
         if(number[i] <= 4){
             if(number[i] % 2 == 0){
                 score += number[i] * 2;
@@ -40,30 +48,29 @@ function updateBlue(){
             score += number[i] + 4;
         }
     }
-    document.getElementById("blueScore").innerHTML = score.toString();
+    blueScore = score;
+    document.getElementById("blueScore").innerHTML = blueScore.toString();
 }
 
 function updateRed(){
-    let number = [];
-    number[0] = parseInt(document.getElementById("r1").textContent);
-    number[1] = parseInt(document.getElementById("r2").textContent);
-    number[2] = parseInt(document.getElementById("r3").textContent);
-    number[3] = parseInt(document.getElementById("r4").textContent);
-    number[4] = parseInt(document.getElementById("r5").textContent);
+    for(var i = 0; i < 5; i++){
+        numOfArrow[1][i] = parseInt(document.getElementById("r" + i + 1).textContent);
+    }
 
-    var score = 0, i = 0;
-    for(i = 0; i < 5; i++){
-        if(number[i] <= 4){
-            if(number[i] % 2 == 0){
-                score += number[i] * 2;
+    var score = 0;
+    for(var i = 0; i < 5; i++){
+        if(numOfArrow[1][i] <= 4){
+            if(numOfArrow[1][i] % 2 == 0){
+                score += numOfArrow[1][i] * 2;
             }else{
-                score += number[i] * 2 - 1;
+                score += numOfArrow[1][i] * 2 - 1;
             }
         }else{
-            score += number[i] + 4;
+            score += numOfArrow[1][i] + 4;
         }
     }
-    document.getElementById("redScore").innerHTML = score.toString();
+    redScore = score;
+    document.getElementById("redScore").innerHTML = redScore.toString();
 }
 
 function reset(){
@@ -74,4 +81,8 @@ function reset(){
     }
     updateRed();
     updateBlue();
+}
+
+function compare(){
+
 }
